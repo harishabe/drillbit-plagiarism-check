bf6700aeb3588c6450a4c2e03abfc428b51dbd4b
harishabe <harish90142@gmail.com>
2022-06-14 22:36:09 +0530

    create instructor design

---
+++ pages/admin/form/InstructorForm.js
@@ -0,0 +1,37 @@
+import Grid from '@mui/material/Grid';
+import { Skeleton } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { FormComponent } from '../../../components';
+import FormJson from '../../../constant/form/instructor-form.json';
+
+function InstructorForm(){
+    const { handleSubmit, control } = useForm({
+        mode: 'all',
+    });
+
+    const onSubmit = (data) => {
+        console.log('shyla-first-drillbit-work',data);  
+    };
+
+    return(
+        <div>
+           <form onSubmit={handleSubmit(onSubmit)}>
+                <Grid container>
+                    {FormJson?.map((field, i) => (
+                        <Grid md={12} style={{ marginLeft: '8px' }}>                                 
+                                <FormComponent
+                                    key={i}
+                                    field={field}
+                                    control={control}
+                                />
+                            
+                        </Grid>
+                    ))}
+                </Grid>
+            </form>
+        </div>
+    )
+    
+
+}
+export default InstructorForm;
