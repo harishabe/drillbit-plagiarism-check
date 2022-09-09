cb9357009c4ee392bc7c97a3ab931d6d64e1dd01
harishabe <harish90142@gmail.com>
2022-09-09 15:20:01 +0530

    changes

--- utils/BaseUrl.js
+++ utils/BaseUrl.js
@@ -3,3 +3,4 @@ export const BASE_URL_EXTREM = 'http://uat.drillbitplagiarismcheck.com:8082';
 export const BASE_URL_UPLOAD = 'http://uat.drillbitplagiarismcheck.com:8083';
 export const BASE_URL_SUPER = 'http://uat.drillbitplagiarismcheck.com:8086';
 export const BASE_URL_INTEGRATION = 'http://uat.drillbitplagiarismcheck.com:8080';
+export const BASE_URL_PRO = ' http://uat.drillbitplagiarismcheck.com:8087/pro';
---
+++ utils/EndPointPro.js
@@ -0,0 +1,5 @@
+const END_POINTS_PRO = {
+    'DASHBOARD_API_URL':'/admin/dashboard'
+}
+
+export default END_POINTS_PRO;
