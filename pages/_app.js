ea9ccd6a0e39a2195d1bf8ed66c97cfff8f5bfa7
harishabe <harish90142@gmail.com>
2022-10-01 11:46:06 +0530

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -54,7 +54,7 @@ export default function MyApp(props) {
 
     function authCheck(url) {
         setUser(localStorage.getItem('token'));
-        const publicPaths = ['/auth/login', '/auth/forgot-password'];
+        const publicPaths = ['/auth/login', '/auth/forgot-password','/auth/reset-password'];
         const path = url.split('?')[0];
         if (!localStorage.getItem('token') && !publicPaths.includes(path)) {
             setAuthorized(false);
