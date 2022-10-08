8e51f0f844b3e5d80a35130582906d2f160e5210
harishabe <harish90142@gmail.com>
2022-10-08 14:50:14 +0530

    changes

--- components/file/GDriveFileUpload.js
+++ components/file/GDriveFileUpload.js
@@ -26,7 +26,7 @@ const GDriveFileUpload = ({
     isLoadingFileDrive,
     title,
     UploadFileDrive,
-    allowedFormat
+    allowedFormat,
     routerObj,
     uploadFileSuccess,
     UploadGdriveFileDataClear
