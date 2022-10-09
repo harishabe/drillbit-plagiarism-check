7214e20dc0768b3ad45cdfa656e055fdd3c742fd
Akshay <akshay.gp@drillbitplagiarism.com>
2022-10-09 17:36:45 +0530

    changes

--- pages/extream/instructor/dashboard.js
+++ pages/extream/instructor/dashboard.js
@@ -80,7 +80,7 @@ const Dashboard = ({
     }, [instructorDashboardData]);
 
     const handlePage = (e, item) => {
-        router.push({ pathname: '/extream/instructor/mysubmissions', query: { isAssignment: true, clasId: item?.class_id, assId: item?.ass_id } });
+        router.push({ pathname: '/extream/instructor/mysubmissions', query: { isAssignment: true, clasId: item?.class_id, assId: item?.ass_id, clasName: item?.class_name, assName: item?.assignment_name } });
     };
 
     return (
