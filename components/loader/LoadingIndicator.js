4f26757cd897cfb4bdd39dc2dbdb1ca583b7b710
harishabe <harish90142@gmail.com>
2022-10-13 00:01:01 +0530

    chagnes

--- components/loader/LoadingIndicator.js
+++ components/loader/LoadingIndicator.js
@@ -1,41 +1,47 @@
 import React from 'react';
+import { DrillBitSymbolLogo } from '../../assets/icon';
+import BeatLoader from 'react-spinners/BeatLoader';
 import styled from 'styled-components';
 
 const LoaderContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    align-content: center;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    align-content:center;
     position: absolute;
     height: 100%;
     width: 100%;
     left: 0;
     right: 0;
-    bottom: 0;
-    z-index: 9999;
+    z-index: 999;
     background: rgba(0, 0, 0, 0.5);
 `;
 
 const LoaderWrapper = styled.div`
-    background:white;
-    padding:20px 20px;
-    border-radius:10px;
-    z-index:999;
+    background: white;
+    padding: 5px 0px;
+    border-radius: 10px;
 `;
 
-const LoadingIndicator = () => {
+const LogoContainer = styled.div`
+    margin-left:75px;
+`;
+
+const BeatContainer = styled.div`
+    text-align:center;
+`;
+
+export const LoadingIndicator = () => {
     return (
         <LoaderContainer>
             <LoaderWrapper>
-                <div className="pulse-container">
-                    <div className="pulse-bubble pulse-bubble-1"></div>
-                    <div className="pulse-bubble pulse-bubble-2"></div>
-                    <div className="pulse-bubble pulse-bubble-3"></div>
-                </div>
-                <p>Please wait , data is loading...</p>
+                <LogoContainer>
+                    <DrillBitSymbolLogo />
+                </LogoContainer>
+                <BeatContainer>
+                    <BeatLoader color="#3672FF" />
+                </BeatContainer>
             </LoaderWrapper>
         </LoaderContainer>
-    );
+    )
 };
-
-export default LoadingIndicator;
--- components/loader/PageChange.js
+++ components/loader/PageChange.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import SquareLoader from 'react-spinners/SquareLoader';
-
+import { LoadingIndicator } from './LoadingIndicator';
 export default function PageChange() {
     return (
-        <div>
-            <div className="page-transition-wrapper-div">
-                <div className="page-transition-icon-wrapper mb-3">
-                    <SquareLoader color='#3672FF' loading={true} size={40} />
-                </div>
-            </div>
-        </div>
+        <LoadingIndicator />
     );
 }
