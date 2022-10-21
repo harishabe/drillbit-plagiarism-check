57f98e66b798425eab04535497ee198ed86b68de
harishabe <harish90142@gmail.com>
2022-10-22 03:14:20 +0530

    changes

--- utils/BaseUrl.js
+++ utils/BaseUrl.js
@@ -4,6 +4,6 @@ export const BASE_URL_UPLOAD = 'https://s1.drillbitplagiarismcheck.com:8083';
 export const BASE_URL_SUPER = 'https://s1.drillbitplagiarismcheck.com:8086/superadmin';
 export const BASE_URL_INTEGRATION = 'https://s1.drillbitplagiarismcheck.com:8080';
 export const BASE_URL_PRO = 'https://s1.drillbitplagiarismcheck.com:8087/pro';
-export const BASE_URL_ANALYSIS = 'https://s1.drillbitplagiarismcheck.com:8083/drillbit-analysis/analysis/';
+export const BASE_URL_ANALYSIS = 'https://s1.drillbitplagiarismcheck.com:8084/drillbit-analysis/analysis/';
 export const BASE_URL_REGIONAL_ANALYSIS = 'https://www.drillbitplagiarismcheck.com:8081/drillbit_regional/viewRegionalResult/';
 //export const BASE_URL_REGIONAL_ANALYSIS = 'https://www.drillbitplagiarismcheck.com/drillbit_regional/uat/viewRegionalResultNew/';
