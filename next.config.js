272b608196d62e2eb6dbc467d72f3d5d7f0c64ee
harishabe <harish90142@gmail.com>
2022-10-29 00:20:37 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,41 +1,41 @@
-// const runtimeCaching = require("next-pwa/cache");
-// const withPWA = require("next-pwa")({
-//   dest: "public",
-//   register: true,
-//   skipWaiting: true,
-//   runtimeCaching,
-// });
-
-// const nextConfig = withPWA({
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-// });
-// module.exports = nextConfig;
-
 const runtimeCaching = require("next-pwa/cache");
-const withPWA = require("next-pwa");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+});
 
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development'
-  },
-  i18n: {
-    locales: ['en', 'fr', 'no'],
-    defaultLocale: 'en',
-  },
-  experimental: {
-    // Enables the styled-components SWC transform
-    styledComponents: true
-  },
+const nextConfig = withPWA({
   eslint: {
     ignoreDuringBuilds: true,
   },
-  runtimeCaching
 });
+module.exports = nextConfig;
+
+// const runtimeCaching = require("next-pwa/cache");
+// const withPWA = require("next-pwa");
+
+// module.exports = withPWA({
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//     disable: process.env.NODE_ENV === 'development'
+//   },
+//   i18n: {
+//     locales: ['en', 'fr', 'no'],
+//     defaultLocale: 'en',
+//   },
+//   experimental: {
+//     // Enables the styled-components SWC transform
+//     styledComponents: true
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   runtimeCaching
+// });
 
 /**
  * Below code working in production
