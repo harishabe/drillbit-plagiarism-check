5fe2accb6f94b06f35730be072aebfd7e90b090e
harishabe <harish90142@gmail.com>
2022-11-02 16:37:39 +0530

    uat uerl

--- utils/BaseUrl.js
+++ utils/BaseUrl.js
@@ -1,9 +1,18 @@
-export const BASE_URL = 'https://s1.drillbitplagiarismcheck.com:8081';
-export const BASE_URL_EXTREM = 'https://s1.drillbitplagiarismcheck.com:8082';
-export const BASE_URL_UPLOAD = 'https://s1.drillbitplagiarismcheck.com:8083';
-export const BASE_URL_SUPER = 'https://s1.drillbitplagiarismcheck.com:8086/superadmin';
-export const BASE_URL_INTEGRATION = 'https://s1.drillbitplagiarismcheck.com:8080';
-export const BASE_URL_PRO = 'https://s1.drillbitplagiarismcheck.com:8087/pro';
-export const BASE_URL_ANALYSIS = 'https://s1.drillbitplagiarismcheck.com:8084/drillbit-analysis/analysis/';
+// export const BASE_URL = 'https://s1.drillbitplagiarismcheck.com:8081';
+// export const BASE_URL_EXTREM = 'https://s1.drillbitplagiarismcheck.com:8082';
+// export const BASE_URL_UPLOAD = 'https://s1.drillbitplagiarismcheck.com:8083';
+// export const BASE_URL_SUPER = 'https://s1.drillbitplagiarismcheck.com:8086/superadmin';
+// export const BASE_URL_INTEGRATION = 'https://s1.drillbitplagiarismcheck.com:8080';
+// export const BASE_URL_PRO = 'https://s1.drillbitplagiarismcheck.com:8087/pro';
+// export const BASE_URL_ANALYSIS = 'https://s1.drillbitplagiarismcheck.com:8084/drillbit-analysis/analysis/';
+// export const BASE_URL_REGIONAL_ANALYSIS = 'https://www.drillbitplagiarismcheck.com:8081/drillbit_regional/viewRegionalResult/';
+
+
+export const BASE_URL = 'https://uat.drillbitplagiarismcheck.com:8081';
+export const BASE_URL_EXTREM = 'https://uat.drillbitplagiarismcheck.com:8082';
+export const BASE_URL_UPLOAD = 'https://uat.drillbitplagiarismcheck.com:8083';
+export const BASE_URL_SUPER = 'https://uat.drillbitplagiarismcheck.com:8086/superadmin';
+export const BASE_URL_INTEGRATION = 'https://uat.drillbitplagiarismcheck.com:8080';
+export const BASE_URL_PRO = 'https://uat.drillbitplagiarismcheck.com:8087/pro';
+export const BASE_URL_ANALYSIS = 'https://uat.drillbitplagiarismcheck.com:8084/drillbit-analysis/analysis/';
 export const BASE_URL_REGIONAL_ANALYSIS = 'https://www.drillbitplagiarismcheck.com:8081/drillbit_regional/viewRegionalResult/';
-//export const BASE_URL_REGIONAL_ANALYSIS = 'https://www.drillbitplagiarismcheck.com/drillbit_regional/uat/viewRegionalResultNew/';
