bf59b8f1a784af6e727e1354fbde20c27911fce2
harishabe <harish90142@gmail.com>
2022-11-09 19:17:03 +0530

    removed port

--- utils/BaseUrl.js
+++ utils/BaseUrl.js
@@ -7,12 +7,11 @@
 // export const BASE_URL_ANALYSIS = 'https://s1.drillbitplagiarismcheck.com:8084/drillbit-analysis/analysis/';
 // export const BASE_URL_REGIONAL_ANALYSIS = 'https://www.drillbitplagiarismcheck.com:8081/drillbit_regional/viewRegionalResult/';
 
-
-export const BASE_URL = `${process.env.NEXT_PUBLIC_SECRET}:8081`;
-export const BASE_URL_EXTREM = `${process.env.NEXT_PUBLIC_SECRET}:8082`;
-export const BASE_URL_UPLOAD = `${process.env.NEXT_PUBLIC_SECRET}:8083`;
-export const BASE_URL_SUPER = `${process.env.NEXT_PUBLIC_SECRET}:8086/superadmin`;
-export const BASE_URL_INTEGRATION = `${process.env.NEXT_PUBLIC_SECRET}:8080`;
-export const BASE_URL_PRO = `${process.env.NEXT_PUBLIC_SECRET}:8087/pro`;
-export const BASE_URL_ANALYSIS = `${process.env.NEXT_PUBLIC_SECRET}:8084/drillbit-analysis/analysis/`;
+export const BASE_URL = `${process.env.NEXT_PUBLIC_SECRET}`;
+export const BASE_URL_EXTREM = `${process.env.NEXT_PUBLIC_SECRET}`;
+export const BASE_URL_UPLOAD = `${process.env.NEXT_PUBLIC_SECRET}`;
+export const BASE_URL_SUPER = `${process.env.NEXT_PUBLIC_SECRET}/superadmin`;
+export const BASE_URL_INTEGRATION = `${process.env.NEXT_PUBLIC_SECRET}`;
+export const BASE_URL_PRO = `${process.env.NEXT_PUBLIC_SECRET}/pro`;
+export const BASE_URL_ANALYSIS = `${process.env.NEXT_PUBLIC_SECRET}/drillbit-analysis/analysis/`;
 export const BASE_URL_REGIONAL_ANALYSIS = 'https://www.drillbitplagiarismcheck.com:8081/drillbit_regional/viewRegionalResult/';
