e2e3fb89b1049bf5818e113e4575dce2544651e8
harishabe <harish90142@gmail.com>
2022-11-10 12:44:03 +0530

    changes

--- utils/BaseUrl.js
+++ utils/BaseUrl.js
@@ -14,5 +14,6 @@ export const BASE_URL_UPLOAD = `${process.env.NEXT_PUBLIC_SECRET}`;
 export const BASE_URL_SUPER = `${process.env.NEXT_PUBLIC_SECRET}/superadmin`;
 export const BASE_URL_INTEGRATION = `${process.env.NEXT_PUBLIC_SECRET}`;
 export const BASE_URL_PRO = `${process.env.NEXT_PUBLIC_SECRET}/pro`;
-export const BASE_URL_ANALYSIS = `${process.env.NEXT_PUBLIC_SECRET}/drillbit-analysis/analysis/`;
+export const BASE_URL_ANALYSIS = `https://uat.drillbitplagiarismcheck.com/drillbit-analysis/analysis/`;
+//export const BASE_URL_ANALYSIS = `${process.env.NEXT_PUBLIC_SECRET}/drillbit-analysis/analysis/`;
 export const BASE_URL_REGIONAL_ANALYSIS = 'https://www.drillbitplagiarismcheck.com:8081/drillbit_regional/viewRegionalResult/';
