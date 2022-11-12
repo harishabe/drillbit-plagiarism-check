2b0a19ea65febb0ca20f901291a937c4d1b77a60
harishabe <harish90142@gmail.com>
2022-11-12 10:28:24 +0530

    changes

--- utils/BaseUrl.js
+++ utils/BaseUrl.js
@@ -1,8 +1,7 @@
-export const BASE_URL = `${process.env.NEXT_PUBLIC_SECRET.includes('uat') ? process.env.NEXT_PUBLIC_SECRET + ':8081' : process.env.NEXT_PUBLIC_SECRET}`;
-export const BASE_URL_EXTREM = `${process.env.NEXT_PUBLIC_SECRET.includes('uat') ? process.env.NEXT_PUBLIC_SECRET + ':8082' : process.env.NEXT_PUBLIC_SECRET}`;
-export const BASE_URL_UPLOAD = `${process.env.NEXT_PUBLIC_SECRET.includes('uat') ? process.env.NEXT_PUBLIC_SECRET + ':8083' : process.env.NEXT_PUBLIC_SECRET}`;
-export const BASE_URL_SUPER = `${process.env.NEXT_PUBLIC_SECRET.includes('uat') ? process.env.NEXT_PUBLIC_SECRET + ':8086/superadmin' : process.env.NEXT_PUBLIC_SECRET}/superadmin`;
-//export const BASE_URL_INTEGRATION = `${process.env.NEXT_PUBLIC_SECRET.includes('uat') ? process.env.NEXT_PUBLIC_SECRET + ':8080' : process.env.NEXT_PUBLIC_SECRET}`;
-export const BASE_URL_PRO = `${process.env.NEXT_PUBLIC_SECRET.includes('uat') ? process.env.NEXT_PUBLIC_SECRET + ':8087' : process.env.NEXT_PUBLIC_SECRET}/pro`;
-export const BASE_URL_ANALYSIS = `${process.env.NEXT_PUBLIC_SECRET.includes('uat') ? process.env.NEXT_PUBLIC_SECRET + ':8084/drillbit-analysis/analysis/' : process.env.NEXT_PUBLIC_SECRET}/drillbit-analysis/analysis/`;
-export const BASE_URL_REGIONAL_ANALYSIS = 'https://www.drillbitplagiarismcheck.com:8081/drillbit_regional/viewRegionalResult/';
+export const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}`;
+export const BASE_URL_EXTREM = `${process.env.NEXT_PUBLIC_BASE_URL_EXTREM}`;
+export const BASE_URL_UPLOAD = `${process.env.NEXT_PUBLIC_BASE_URL_UPLOAD}`;
+export const BASE_URL_SUPER = `${process.env.NEXT_PUBLIC_BASE_URL_SUPER}`;
+export const BASE_URL_PRO = `${process.env.NEXT_PUBLIC_BASE_URL_PRO}`;
+export const BASE_URL_ANALYSIS = `${process.env.NEXT_PUBLIC_BASE_URL_ANALYSIS}`;
+export const BASE_URL_REGIONAL_ANALYSIS = `${process.env.NEXT_PUBLIC_BASE_URL_REGIONAL_ANALYSIS}`
