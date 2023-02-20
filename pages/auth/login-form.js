1395cecee7231f24d794158708cd1c6838cdb4a4
harishabe <harish90142@gmail.com>
2023-02-20 22:33:51 +0530

    changes

--- pages/auth/login-form.js
+++ pages/auth/login-form.js
@@ -1,96 +1,97 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { useRouter } from 'next/router';
-import { useForm } from 'react-hook-form';
-import { FormComponent } from '../../components';
-import FormJson from '../../constant/form/login-form.json';
-import { Role } from '../../constant/data';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import { FormComponent } from "../../components";
+import FormJson from "../../constant/form/login-form.json";
+import { Role } from "../../constant/data";
 
-import { login } from '../../redux/action/login/LoginAction';
+import { login } from "../../redux/action/login/LoginAction";
 
-const LoginForm = ({
-    login,
-    loginState,
-    isLoading
-}) => {
-    const router = useRouter();
+const LoginForm = ({ login, loginState, isLoading }) => {
+  const router = useRouter();
 
-    const { handleSubmit, control } = useForm({
-        mode: 'all',
-    });
+  const { handleSubmit, control } = useForm({
+    mode: "all",
+  });
 
-    const onSubmit = (data) => {
-        login(data);
-    };
+  const onSubmit = (data) => {
+    login(data);
+  };
 
-    useEffect(() => {
-        if (loginState?.role === Role.admin) {
-            localStorage.setItem('role', Role.admin);
-            localStorage.setItem('token', loginState?.token);
-            localStorage.setItem('email', loginState?.username);
-            localStorage.setItem('name', loginState?.name);
-            router.push('/extream/admin/dashboard');
-        } else if (loginState?.role === Role.instructor) {
-            localStorage.setItem('role', Role.instructor);
-            localStorage.setItem('token', loginState?.token);
-            localStorage.setItem('email', loginState?.username);
-            localStorage.setItem('name', loginState?.name);
-            router.push('/extream/instructor/dashboard');
-        } else if (loginState?.role === Role.student) {
-            localStorage.setItem('role', Role.student);
-            localStorage.setItem('token', loginState?.token);
-            localStorage.setItem('email', loginState?.username);
-            localStorage.setItem('name', loginState?.name);
-            router.push('/extream/student/dashboard');
-        } else if (loginState?.role === Role.super) {
-            localStorage.setItem('role', Role.super);
-            localStorage.setItem('token', loginState?.token);
-            localStorage.setItem('email', loginState?.username);
-            localStorage.setItem('name', loginState?.name);
-            router.push('/super/dashboard');
-        } else if (loginState?.role === Role.proAdmin) {
-            localStorage.setItem('role', Role.proAdmin);
-            localStorage.setItem('token', loginState?.token);
-            localStorage.setItem('email', loginState?.username);
-            localStorage.setItem('name', loginState?.name);
-            router.push('/pro/admin/dashboard');
-        } else if (loginState?.role === Role.supplier) {
-            localStorage.setItem('role', Role.supplier);
-            localStorage.setItem('token', loginState?.token);
-            localStorage.setItem('email', loginState?.username);
-            localStorage.setItem('name', loginState?.name);
-            router.push('/supplier/extreamProduct');
-        }
-    }, [router,loginState]);
+  useEffect(() => {
+    if (loginState?.role === Role.admin) {
+      localStorage.setItem("role", Role.admin);
+      localStorage.setItem("token", loginState?.token);
+      localStorage.setItem("email", loginState?.username);
+      localStorage.setItem("name", loginState?.name);
+      router.push("/extream/admin/dashboard");
+    } else if (loginState?.role === Role.instructor) {
+      localStorage.setItem("role", Role.instructor);
+      localStorage.setItem("token", loginState?.token);
+      localStorage.setItem("email", loginState?.username);
+      localStorage.setItem("name", loginState?.name);
+      router.push("/extream/instructor/dashboard");
+    } else if (loginState?.role === Role.student) {
+      localStorage.setItem("role", Role.student);
+      localStorage.setItem("token", loginState?.token);
+      localStorage.setItem("email", loginState?.username);
+      localStorage.setItem("name", loginState?.name);
+      router.push("/extream/student/dashboard");
+    } else if (loginState?.role === Role.proAdmin) {
+      localStorage.setItem("role", Role.proAdmin);
+      localStorage.setItem("token", loginState?.token);
+      localStorage.setItem("email", loginState?.username);
+      localStorage.setItem("name", loginState?.name);
+      router.push("/pro/admin/dashboard");
+    } else if (loginState?.role === Role.proUser) {
+      localStorage.setItem("role", Role.proUser);
+      localStorage.setItem("token", loginState?.token);
+      localStorage.setItem("email", loginState?.username);
+      localStorage.setItem("name", loginState?.name);
+      router.push("/pro/user/dashboard");
+    } else if (loginState?.role === Role.super) {
+      localStorage.setItem("role", Role.super);
+      localStorage.setItem("token", loginState?.token);
+      localStorage.setItem("email", loginState?.username);
+      localStorage.setItem("name", loginState?.name);
+      router.push("/super/dashboard");
+    } else if (loginState?.role === Role.supplier) {
+      localStorage.setItem("role", Role.supplier);
+      localStorage.setItem("token", loginState?.token);
+      localStorage.setItem("email", loginState?.username);
+      localStorage.setItem("name", loginState?.name);
+      router.push("/supplier/extreamProduct");
+    }
+  }, [router, loginState]);
 
-    return (
-        <>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                {
-                    FormJson ?
-                        FormJson.map((field, i) =>
-                            <FormComponent
-                                key={i}
-                                field={field}
-                                control={control}
-                                isLoading={isLoading}
-                            />) : null
-                }
-            </form>
-        </>
-    );
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {FormJson
+          ? FormJson.map((field, i) => (
+              <FormComponent
+                key={i}
+                field={field}
+                control={control}
+                isLoading={isLoading}
+              />
+            ))
+          : null}
+      </form>
+    </>
+  );
 };
 
-
 const mapStateToProps = (state) => ({
-    loginState: state?.login?.data,
-    isLoading: state?.login?.isLoadingLogin
+  loginState: state?.login?.data,
+  isLoading: state?.login?.isLoadingLogin,
 });
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        login: (values) => dispatch(login(values))
-    };
+  return {
+    login: (values) => dispatch(login(values)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
