7b7cf91669b89d94b8f7341b7d4310735b0b6156
Akshay <akshay.gp@drillbitplagiarism.com>
2023-03-14 12:23:13 +0530

    changes

--- pages/extream/instructor/form/MyclassesForm.js
+++ pages/extream/instructor/form/MyclassesForm.js
@@ -26,7 +26,7 @@ const MyClassesForm = ({
 
     const expiryDate = useWatch({
         control,
-        name: 'expiry_date',
+        name: 'end_date',
     });
 
     useEffect(() => {
@@ -79,12 +79,10 @@ const MyClassesForm = ({
 
     const onSubmit = (data) => {
         if (editOperation) {
-            data['end_date'] = convertDate(data.expiry_date);
-            delete data.expiry_date;
+            data['end_date'] = convertDate(data.end_date);
             EditClass(editData?.id, data);
         } else {
-            let DetailedData = { ...data, 'end_date': convertDate(data.expiry_date) };
-            delete DetailedData.expiry_date;
+            let DetailedData = { ...data, 'end_date': convertDate(data.end_date) };
             CreateClass(DetailedData);
         }
     };
@@ -110,12 +108,12 @@ const MyClassesForm = ({
             let a = {
                 'class_name': editData.name,
                 'description': editData.description,
-                'expiry_date': editData.expiry_date,
+                'end_date': editData.end_date,
             };
             const fields = [
                 'class_name',
                 'description',
-                'expiry_date',
+                'end_date',
             ];
             fields.forEach(field => setValue(field, a[field]));
             modifyFormField('Edit Class', true);
