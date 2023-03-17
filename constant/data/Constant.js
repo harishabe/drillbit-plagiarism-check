14faed45c7cf209ce0713f416fb144a06c1e0779
Akshay <akshay.gp@drillbitplagiarism.com>
2023-03-17 14:19:08 +0530

    file formats length, size, max limit work

--- constant/data/Constant.js
+++ constant/data/Constant.js
@@ -166,15 +166,42 @@ export const UPLOAD_TITLE_CONSTANT = {
 }
 
 export const UPLOAD_SUPPORTED_FILES = {
-    'SINGLE': 'File formats: pdf, doc, docx, txt, dotx, dot, pptx, xlsx, wpd, rtf, html, odt, ppt, xls, ps, tex, xml, tiff',
-    'SCANNED_PDF': 'File formats: pdf',
-    'REGIONAL_FORMAT': 'File formats: pdf, docx, txt, dotx, dot, doc, html, odt, tex, xml, tiff',
-    'ZIP': 'Supported Compressed file format: zip',
+    'SINGLE': {
+        'FILE_FORMATS': 'File formats: pdf, doc, docx, txt, dotx, dot, pptx, xlsx, wpd, rtf, html, odt, ppt, xls, ps, tex, xml, tiff',
+        'LENGTH': 'Document length - upto 800 pages',
+        'SIZE': 'Document size - upto 100 MB',
+        'MAX_FILES': ' Multiple files - upto 15 files at a time'
+    },
+    'SCANNED_PDF': {
+        'FILE_FORMATS': 'File formats: pdf',
+        'LENGTH': 'Document length - upto 800 pages',
+        'SIZE': 'Document size - upto 100 MB',
+    },
+    'REGIONAL_FORMAT': {
+        'FILE_FORMATS': 'File formats: pdf, docx, txt, dotx, dot, doc, html, odt, tex, xml, tiff',
+        'LENGTH': 'Document length - upto 800 pages',
+        'SIZE': 'Document size - upto 100 MB',
+    },
+    'ZIP': {
+        'FILE_FORMATS': 'Supported Compressed file format: zip',
+        'LENGTH': 'Document length - upto 800 pages',
+        'SIZE': 'Document size - upto 100 MB',
+        'MAX_FILES': 'Documents - upto 100 files in a single zip'
+    },
     'NON_ZIP': 'Non-Supported Compressed file format : rar,tar,7z',
-    'GDRIVE': 'File formats: pdf, doc, docx, txt, dotx, dot, pptx, xlsx, wpd, rtf, html, odt, ppt, xls, ps, tex, xml, tiff',
+    'GDRIVE': {
+        'FILE_FORMATS': 'File formats: pdf, doc, docx, txt, dotx, dot, pptx, xlsx, wpd, rtf, html, odt, ppt, xls, ps, tex, xml, tiff',
+        'LENGTH': 'Document length - upto 800 pages',
+        'SIZE': 'Document size - upto 100 MB',
+    },
     'NON_ENGLISH': 'Supported file format: pdf, doc, docx, txt, dotx, dot, pptx, xlsx, wpd, rtf, html, odt, ppt, xls, ps, tex, xml, tiff',
     'REGIONAL': 'Supported file format: docx',
-    'REPO_ALLOWED_FILE': 'File formats: pdf, doc, docx',
+    'REPO_ALLOWED_FILE': {
+        'FILE_FORMATS': 'File formats: pdf, doc, docx',
+        'LENGTH': 'Document length - upto 800 pages',
+        'SIZE': 'Document size - upto 100 MB',
+        'MAX_FILES': ' Multiple files - upto 15 files at a time'
+    },
     'INVALID_FILE_FORMAT_ERROR':'Invalid file format. Please check above file formats'
 }
 
