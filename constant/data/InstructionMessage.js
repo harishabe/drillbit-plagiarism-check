581a65a23ce5d84d2d23c43b096bdf7ced460fb3
Akshay <akshay.gp@drillbitplagiarism.com>
2023-04-18 13:42:58 +0530

    changes

--- constant/data/InstructionMessage.js
+++ constant/data/InstructionMessage.js
@@ -3,7 +3,7 @@ export const INSTRUCTIONS_STEPS = {
         'STEP_1': 'Create a new folder by clicking the "+" icon, then select the folder by clicking the arrow, and upload a file for plagiarism checking.'
     },
     'CLASS': {
-        'STEP_1': 'Create a new class by clicking "+", provide class name, description, and expiry date, then select the class by clicking the arrow to create assignments and enroll students.'
+        'STEP_1': 'Create a new class by clicking "+" icon, provide class name, description, and expiry date, then select the class by clicking the arrow to create assignments and enroll students.'
     },
     'SUBMISSION': {
         'STEP_1': 'Click on "+" icon, select language, upload type, browse, enter details and submit it, and then view similarity percentage result by refreshing the page.'
@@ -16,9 +16,9 @@ export const INSTRUCTIONS_STEPS = {
         'STEP_1': 'Before creating a new assignment, enroll students under the "Students" tab, then click the "+ icon" to create the assignment. Select the assignment (click the arrow) to view student submissions or upload files for plagiarism checking.'
     },
     'STUDENT': {
-        'STEP_1': 'student page - Enroll students in a class by clicking the "+Students" icon, add single students or multiple students using a template, and then create assignments under the "Assignment" tab',
+        'STEP_1': 'Enroll students in a class by clicking the "+" icon, add single students or multiple students using a template, and then create assignments under the "Assignment" tab',
     },
     'STUDENT_SUBMISSION': {
-        'STEP_1': 'Upload a file by clicking "+icon", select the language, browse and select the file, and view the similarity percentage after refreshing.',
+        'STEP_1': 'Upload a file by clicking "+" icon, select the language, browse and select the file, and view the similarity percentage after refreshing.',
     }
 }
