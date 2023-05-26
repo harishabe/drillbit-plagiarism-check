fb3a4c0cfb5e8c351d48f950a72d5ce773975cc3
Akshay <akshay.gp@drillbitplagiarism.com>
2023-05-26 17:55:13 +0530

    UI fix

--- components/card/CardInfoView.js
+++ components/card/CardInfoView.js
@@ -271,7 +271,7 @@ const CardInfoView = ({
                 <Divider />
                 <CardActions style={ { padding: '18px' } }>
                     <Grid container>
-                        <Grid item md={ 9 } xs={ 9 }>
+                        <Grid item md={ isConfig ? 8 : 9 } xs={ isConfig ? 8 : 9 }>
                             { isTimer &&
                                 <CardStatusColor
                                     style={ { borderRadius: '3px' } }
@@ -296,7 +296,7 @@ const CardInfoView = ({
                             }
                         </Grid>
 
-                        <Grid className={ classes.right } item md={ 3 } xs={ 3 }>
+                        <Grid className={ classes.right } item md={ isConfig ? 4 : 3 } xs={ isConfig ? 4 : 3 }>
                             { isSubmit ?
                                 <Link href={ submitPath }>
                                     <Button
