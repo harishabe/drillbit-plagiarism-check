cab760ba8461aac6d6e2a896772ab1f957e08ed7
Akshay <akshay.gp@drillbitplagiarism.com>
2023-06-16 12:57:42 +0530

    changes

--- components/ellipsis/EllipsisText.js
+++ components/ellipsis/EllipsisText.js
@@ -32,149 +32,56 @@
 //     }
 // }
 
-// export default EllipsisText;
-
-
-// import React, { Component } from 'react';
-// import { Typography, Tooltip, Snackbar } from '@mui/material';
-
-// class EllipsisText extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.textElement = React.createRef();
-//     }
-
-//     state = {
-//         overflowed: false,
-//         showSnackBar: false,
-//     };
-
-//     componentDidMount() {
-//         this.checkOverflow();
-//         window.addEventListener('resize', this.checkOverflow);
-//     }
-
-//     componentWillUnmount() {
-//         window.removeEventListener('resize', this.checkOverflow);
-//     }
-
-//     checkOverflow = () => {
-//         const { offsetWidth, scrollWidth } = this.textElement.current;
-//         const overflowed = scrollWidth > offsetWidth;
-//         this.setState({ overflowed });
-//     };
-
-//     render() {
-//         const { value, variant, maxWidth } = this.props;
-//         const { overflowed, showSnackBar } = this.state;
-
-//         const truncatedValue = value && overflowed ? value.slice(0, value.length - 3) + '...' : value;
-//         const filledValue = truncatedValue?.padEnd(value.length, ' ');
-
-//         const containerStyle = {
-//             maxWidth: maxWidth || '100px',
-//             overflowX: 'hidden',
-//             tableLayout: 'auto', // Added this line
-//         };
-
-//         return (
-//             <div style={ containerStyle }>
-//                 <Tooltip title={ overflowed ? value : '' } arrow>
-//                     <Typography
-//                         variant={ variant || 'h4' }
-//                         component="div"
-//                         gutterBottom
-//                         ref={ this.textElement }
-//                         style={ { textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden' } }
-//                     >
-//                         { } { value }
-//                     </Typography>
-//                 </Tooltip>
-//                 <Snackbar
-//                     open={ showSnackBar }
-//                     onClose={ () => this.setState({ showSnackBar: false }) }
-//                     message="Text overflowed"
-//                 />
-//             </div>
-//         );
-//     }
-// }
-
-// export default EllipsisText;
+// export default EllipsisText
 
 import React, { useRef, useEffect, useState } from 'react';
-import { Typography, Tooltip, Snackbar } from '@mui/material';
+import { Typography, Tooltip } from '@mui/material';
 
-const EllipsisText = ({ value, variant, tableHeader }) => {
+const EllipsisText = ({ value, variant }) => {
     const textElement = useRef(null);
-    const [overflowed, setOverflowed] = useState(false);
-    const [showSnackBar, setShowSnackBar] = useState(false);
-    const [maxWidth, setMaxWidth] = useState(calculateMaxWidth());
-
-    function calculateMaxWidth() {
-        if (typeof window !== 'undefined') {
-            const zoomPercentage = window.innerWidth / window.outerWidth * 100; // Calculate window zoom percentage
-
-            let maxWidth = 50; // Default maxWidth value
-
-            if (tableHeader !== undefined) {
-                switch (true) {
-                    case tableHeader.length >= 5 && tableHeader.length <= 8:
-                        maxWidth = 100;
-                        break;
-                    case tableHeader.length > 8 && tableHeader.length <= 14:
-                        maxWidth = 150;
-                        break;
-                    case tableHeader.length > 14:
-                        maxWidth = 200;
-                        break;
-                    default:
-                        maxWidth = 100;
-                }
-            }
-
-            // Ensure maxWidth is within the range of 50 to 200
-            maxWidth = Math.max(50, Math.min(200, maxWidth));
-
-            console.log('maxWidth', maxWidth);
-            return `${maxWidth}px`;
-        }
-    }
+    const [maxWidth, setMaxWidth] = useState(100);
+    const [prevZoomPercentage, setPrevZoomPercentage] = useState(typeof window !== 'undefined' && window.innerWidth);
+    const [zoomPercentage, setZoomPercentage] = useState(typeof window !== 'undefined' && window.innerWidth);
 
     useEffect(() => {
-        checkOverflow();
+        const handleResize = () => {
+            setZoomPercentage(typeof window !== 'undefined' && window.innerWidth);
+        };
+
+        handleResize();
         window.addEventListener('resize', handleResize);
+
         return () => {
             window.removeEventListener('resize', handleResize);
         };
     }, []);
 
     useEffect(() => {
-        setMaxWidth(calculateMaxWidth());
-    }, [tableHeader]);
+        setPrevZoomPercentage(prevZoomPercentage => {
+            if (zoomPercentage !== prevZoomPercentage) {
+                return zoomPercentage;
+            }
+            return prevZoomPercentage;
+        });
+    }, [zoomPercentage]);
 
-    const handleResize = () => {
-        setMaxWidth(calculateMaxWidth());
-        checkOverflow();
-    };
-
-    const checkOverflow = () => {
-        const { offsetWidth, scrollWidth } = textElement.current;
-        setOverflowed(scrollWidth > offsetWidth);
-    };
-
-    const truncatedValue = value && overflowed ? value.slice(0, value.length - 3) + '...' : value;
-    const filledValue = truncatedValue?.padEnd(value.length, ' ');
+    useEffect(() => {
+        if (zoomPercentage > prevZoomPercentage) {
+            setMaxWidth(prevMaxWidth => Math.min(prevMaxWidth + 15, 200));
+        } else if (zoomPercentage < prevZoomPercentage) {
+            setMaxWidth(prevMaxWidth => Math.max(prevMaxWidth - 15, 50));
+        }
+    }, [zoomPercentage, prevZoomPercentage]);
 
     const containerStyle = {
-        maxWidth: maxWidth,
+        maxWidth: `${maxWidth}px`,
         overflowX: 'hidden',
         tableLayout: 'auto',
     };
 
     return (
         <div style={ containerStyle }>
-            <Tooltip title={ overflowed ? value : '' } arrow>
+            <Tooltip title={ value } arrow>
                 <Typography
                     variant={ variant || 'h4' }
                     component="div"
@@ -185,16 +92,8 @@ const EllipsisText = ({ value, variant, tableHeader }) => {
                     { value }
                 </Typography>
             </Tooltip>
-            <Snackbar
-                open={ showSnackBar }
-                onClose={ () => setShowSnackBar(false) }
-                message="Text overflowed"
-            />
         </div>
     );
 };
 
 export default EllipsisText;
-
-
-
--- pages/extream/admin/dashboard.js
+++ pages/extream/admin/dashboard.js
@@ -416,10 +416,10 @@ const Dashboard = ({
           <Grid item md={4} xs={12}>
             <CardView>
               <Grid container>
-                <Grid item md={6.6} xs={12}>
+                <Grid item md={ 7.6 } xs={ 12 }>
                   <Heading title="Account Validity" />
                 </Grid>
-                <Grid item md={5.4} xs={12}>
+                <Grid item md={ 4.4 } xs={ 12 }>
                   {isLoadingDashboard ? (
                     <Skeleton />
                   ) : (
@@ -479,10 +479,10 @@ const Dashboard = ({
           <Grid item md={4} xs={12}>
             <CardView>
               <Grid container>
-                <Grid item md={6.6} xs={12}>
+                <Grid item md={ 7.6 } xs={ 12 }>
                   <Heading title="Trend Analysis" />
                 </Grid>
-                <Grid item md={5.4} xs={12}>
+                <Grid item md={ 4.4 } xs={ 12 }>
                   {isLoadingDashboard ? (
                     <Skeleton />
                   ) : (
