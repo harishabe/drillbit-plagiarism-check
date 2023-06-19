48834e486f05404c454a4916dce9cafdd498e992
Akshay <akshay.gp@drillbitplagiarism.com>
2023-06-19 15:23:30 +0530

    Customized ellipsis component

--- components/ellipsis/EllipsisText.js
+++ components/ellipsis/EllipsisText.js
@@ -1,3 +1,85 @@
+import React, { useRef, useEffect, useState } from 'react';
+import { Typography, Tooltip } from '@mui/material';
+
+const EllipsisText = ({ value, variant }) => {
+    const textElement = useRef(null);
+    const [maxWidth, setMaxWidth] = useState(() => {
+        const screenWidth = typeof window !== 'undefined' ? window.innerWidth : 0;
+
+        if (screenWidth >= 800 && screenWidth <= 999) {
+            return 66;
+        } else if (screenWidth >= 1000 && screenWidth <= 1199) {
+            return 83;
+        } else if (screenWidth >= 1200 && screenWidth <= 1399) {
+            return 100;
+        } else if (screenWidth >= 1400 && screenWidth <= 1599) {
+            return 120;
+        } else if (screenWidth >= 1600) {
+            return 150;
+        } else {
+            return 180;
+        }
+    });
+    const [prevZoomPercentage, setPrevZoomPercentage] = useState(typeof window !== 'undefined' && window.innerWidth);
+    const [zoomPercentage, setZoomPercentage] = useState(typeof window !== 'undefined' && window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setZoomPercentage(typeof window !== 'undefined' && window.innerWidth);
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    useEffect(() => {
+        setPrevZoomPercentage(prevZoomPercentage => {
+            if (zoomPercentage !== prevZoomPercentage) {
+                return zoomPercentage;
+            }
+            return prevZoomPercentage;
+        });
+    }, [zoomPercentage]);
+
+    useEffect(() => {
+        if (zoomPercentage > prevZoomPercentage) {
+            setMaxWidth(prevMaxWidth => Math.min(prevMaxWidth + 17, 200));
+        } else if (zoomPercentage < prevZoomPercentage) {
+            setMaxWidth(prevMaxWidth => Math.max(prevMaxWidth - 17, 50));
+        }
+    }, [zoomPercentage, prevZoomPercentage]);
+
+    const containerStyle = {
+        maxWidth: `${maxWidth}px`,
+        overflowX: 'hidden',
+        tableLayout: 'auto',
+    };
+
+    return (
+        <div style={ containerStyle }>
+            <Tooltip title={ value } arrow>
+                <Typography
+                    variant={ variant || 'h4' }
+                    component="div"
+                    gutterBottom
+                    ref={ textElement }
+                    style={ { textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden' } }
+                >
+                    { value }
+                </Typography>
+            </Tooltip>
+        </div>
+    );
+};
+
+export default EllipsisText;
+
+// ---------------------- PREVIOUS CODE---------------------------
+
 // import React, { Component } from 'react';
 // import { Typography, Tooltip } from '@mui/material';
 
@@ -33,67 +115,3 @@
 // }
 
 // export default EllipsisText
-
-import React, { useRef, useEffect, useState } from 'react';
-import { Typography, Tooltip } from '@mui/material';
-
-const EllipsisText = ({ value, variant }) => {
-    const textElement = useRef(null);
-    const [maxWidth, setMaxWidth] = useState(100);
-    const [prevZoomPercentage, setPrevZoomPercentage] = useState(typeof window !== 'undefined' && window.innerWidth);
-    const [zoomPercentage, setZoomPercentage] = useState(typeof window !== 'undefined' && window.innerWidth);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setZoomPercentage(typeof window !== 'undefined' && window.innerWidth);
-        };
-
-        handleResize();
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
-    useEffect(() => {
-        setPrevZoomPercentage(prevZoomPercentage => {
-            if (zoomPercentage !== prevZoomPercentage) {
-                return zoomPercentage;
-            }
-            return prevZoomPercentage;
-        });
-    }, [zoomPercentage]);
-
-    useEffect(() => {
-        if (zoomPercentage > prevZoomPercentage) {
-            setMaxWidth(prevMaxWidth => Math.min(prevMaxWidth + 15, 200));
-        } else if (zoomPercentage < prevZoomPercentage) {
-            setMaxWidth(prevMaxWidth => Math.max(prevMaxWidth - 15, 50));
-        }
-    }, [zoomPercentage, prevZoomPercentage]);
-
-    const containerStyle = {
-        maxWidth: `${maxWidth}px`,
-        overflowX: 'hidden',
-        tableLayout: 'auto',
-    };
-
-    return (
-        <div style={ containerStyle }>
-            <Tooltip title={ value } arrow>
-                <Typography
-                    variant={ variant || 'h4' }
-                    component="div"
-                    gutterBottom
-                    ref={ textElement }
-                    style={ { textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden' } }
-                >
-                    { value }
-                </Typography>
-            </Tooltip>
-        </div>
-    );
-};
-
-export default EllipsisText;
