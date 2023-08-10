fe8bb7b96efae8bcb685aa85cabd5eb8d7132700
harishabe <harish90142@gmail.com>
2023-08-10 16:21:05 +0530

    chagnes

--- theme/drillbit/font.js
+++ theme/drillbit/font.js
@@ -12,6 +12,7 @@ const font = {
         fontSize: '22px',
         fontStyle: 'normal',
         lineHeight: '36px',
+        fontWeight:900,
         letterSpacing: '0.15px'
     },
     h2: {
@@ -19,18 +20,21 @@ const font = {
         fontSize: '18px',
         fontStyle: 'normal',
         lineHeight: '28px',
+        fontWeight:800,
         color: '#666666'
     },
     h3: {
         fontFamily: fontName,
         fontSize: '16px',
         lineHeight: 1.167,
+        fontWeight:700,
         letterSpacing: '0em'
     },
     h3_1: {
         fontFamily: fontName,
         fontSize: '16px',
         lineHeight: 1.167,
+        fontWeight:600,
         letterSpacing: '0em',
         color: '#000'
     },
@@ -38,6 +42,7 @@ const font = {
         fontFamily: fontName,
         fontStyle: 'normal',
         fontSize: '14px',
+        fontWeight:600,
         lineHeight: '24px',
         letterSpacing: '0.5px',
         color: '#666666'
@@ -46,6 +51,7 @@ const font = {
         fontFamily: fontName,
         fontStyle: 'normal',
         fontSize: '14px',
+        fontWeight:600,
         lineHeight: '24px',
         letterSpacing: '0.5px',
         color: '#000'
@@ -54,34 +60,34 @@ const font = {
         fontFamily: fontName,
         fontSize: '14px',
         lineHeight: 1.5,
+        fontWeight:700,
         letterSpacing: '0.00938em'
     },
     body2: {
         fontFamily: fontName,
         fontSize: '12px',
+        fontWeight:500,
         lineHeight: '20px',
         color: '#666666'
     },
     body2_1: {
         fontFamily: fontName,
         fontSize: '14px',
+        fontWeight:600,
         lineHeight: '20px',
         color: '#666666'
     },
     body2_2: {
-        // fontFamily: fontName,
-        // fontWeight: 600,
-        // fontSize: '15px',
-        // lineHeight: '20px',
-        // color: '#666666'
         fontFamily: fontName,
-        fontSize: '14px',
+        fontSize: '15px',
+        fontWeight:900,
         lineHeight: '20px',
         color: '#666666'
     },
     body2_3: {
         fontFamily: fontName,
         fontSize: '14px',
+        fontWeight:600,
         color: '#666666'
         //fontSize: '15px',
     }
