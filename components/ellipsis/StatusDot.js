5e85acd6ceb626fbef25c5e46cfb306234298f5f
Akshay <akshay.gp@drillbitplagiarism.com>
2023-08-18 12:54:03 +0530

    status dot changes

--- components/ellipsis/StatusDot.js
+++ components/ellipsis/StatusDot.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 import { StatusDotIcon } from '../../assets/icon';
-import { Title1 } from '../../components';
+import { EllipsisText } from '../../components';
+import { Typography } from '@mui/material';
 
 const InlineAlign = styled.div`
     display:flex
 `;
 
 const SubTitleStyle = styled.div`
-    margin-top: -8px; 
+    margin-top: -3px; 
     margin-left: 8px;
     text-transform: capitalize;
+`;
+const DotContainer = styled.div`
+    margin-top: -2px; 
 `;
 
 const StatusDot = ({
@@ -19,7 +23,15 @@ const StatusDot = ({
 }) => {
     return (
         <InlineAlign>
-            <StatusDotIcon color={ color } /> <SubTitleStyle><Title1 title={ title?.charAt(0).toUpperCase() + title?.slice(1).substring(0, title?.length).toLowerCase() } /></SubTitleStyle>
+            <DotContainer>
+                <StatusDotIcon color={ color } />
+            </DotContainer>
+            <SubTitleStyle>
+                <EllipsisText
+                    variant='body2_1'
+                    value={ title?.charAt(0).toUpperCase() + title?.slice(1).substring(0, title?.length).toLowerCase() }
+                />
+            </SubTitleStyle>
         </InlineAlign>
     );
 };
