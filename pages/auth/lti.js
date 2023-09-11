2cadb538d0fe2cdb5118703d441537ffee2e0065
harishabe <harish90142@gmail.com>
2023-09-11 22:07:14 +0530

    lti

--- pages/auth/lti.js
+++ pages/auth/lti.js
@@ -1,96 +1,49 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import { Role } from "../../constant/data";
-import PageChange from "../../components/loader/PageChange";
-import ReactDOM from "react-dom";
-import {
-  setItemSessionStorage,
-  getItemSessionStorage,
-} from "../../utils/RegExp";
+import { setItemSessionStorage } from "../../utils/RegExp";
+import PageChange from '../../components/loader/PageChange';
 
 const Lti = () => {
   const router = useRouter();
-  const [loadingPage, isLoadingPage] = useState(true);
-  const [loginState, isLoginState] = useState([]);
-
-  if (
-    getItemSessionStorage("role") &&
-    getItemSessionStorage("token") &&
-    getItemSessionStorage("email") &&
-    getItemSessionStorage("name")
-  ) {
-    isLoadingPage(false);
-  }
 
   useEffect(() => {
-    if (loadingPage) {
-      isLoginState({
-        role: router?.query?.role,
-        token: router?.query?.token,
-        username: router?.query?.username,
-        name: router?.query?.name,
-        message: router?.query?.message,
-      });
-      document.body.classList.add("body-page-transition");
-      ReactDOM.render(
-        <PageChange />,
-        document.getElementById("page-transition")
-      );
-    } else {
-      ReactDOM.unmountComponentAtNode(
-        document.getElementById("page-transition")
-      );
-      document.body.classList.remove("body-page-transition");
-    }
-  }, [router, loadingPage]);
-
-  useEffect(() => {
-    if (loginState?.role === Role.admin) {
+    if (router?.query?.role === Role.admin) {
       setItemSessionStorage("role", Role.admin);
-      setItemSessionStorage("token", loginState?.token);
-      setItemSessionStorage("email", loginState?.username);
-      setItemSessionStorage("name", loginState?.name);
-      router.push(`/extream/admin/dashboard?message=${loginState?.message}`);
-    } else if (loginState?.role === Role.instructor) {
+      setItemSessionStorage("token", router?.query?.token);
+      setItemSessionStorage("email", router?.query?.username);
+      setItemSessionStorage("name", router?.query?.name);
+      setItemSessionStorage("switchRole", "instructor");
+      router.push(`/extream/instructor/myclasses?message=${router?.query?.message}`);
+    } else if (router?.query?.role === Role.instructor) {
       setItemSessionStorage("role", Role.instructor);
-      setItemSessionStorage("token", loginState?.token);
-      setItemSessionStorage("email", loginState?.username);
-      setItemSessionStorage("name", loginState?.name);
-      router.push(
-        `/extream/instructor/dashboard?message=${loginState?.message}`
-      );
-    } else if (loginState?.role === Role.student) {
+      setItemSessionStorage("token", router?.query?.token);
+      setItemSessionStorage("email", router?.query?.username);
+      setItemSessionStorage("name", router?.query?.name);
+      router.push(`/extream/instructor/myclasses?message=${router?.query?.message}`);
+    } else if (router?.query?.role === Role.student) {
       setItemSessionStorage("role", Role.student);
-      setItemSessionStorage("token", loginState?.token);
-      setItemSessionStorage("email", loginState?.username);
-      setItemSessionStorage("name", loginState?.name);
-      router.push(`/extream/student/dashboard?message=${loginState?.message}`);
-    } else if (loginState?.role === Role.proAdmin) {
+      setItemSessionStorage("token", router?.query?.token);
+      setItemSessionStorage("email", router?.query?.username);
+      setItemSessionStorage("name", router?.query?.name);
+      router.push(`/extream/student/myclasses?message=${router?.query?.message}`);
+    } else if (router?.query?.role === Role.proAdmin) {
       setItemSessionStorage("role", Role.proAdmin);
-      setItemSessionStorage("token", loginState?.token);
-      setItemSessionStorage("email", loginState?.username);
-      setItemSessionStorage("name", loginState?.name);
-      router.push(`/pro/admin/dashboard?message=${loginState?.message}`);
-    } else if (loginState?.role === Role.proUser) {
+      setItemSessionStorage("token", router?.query?.token);
+      setItemSessionStorage("email", router?.query?.username);
+      setItemSessionStorage("name", router?.query?.name);
+      setItemSessionStorage("switchProRole", "user");
+      router.push(`/pro/user/myfolder?message=${router?.query?.message}`);
+    } else if (router?.query?.role === Role.proUser) {
       setItemSessionStorage("role", Role.proUser);
-      setItemSessionStorage("token", loginState?.token);
-      setItemSessionStorage("email", loginState?.username);
-      setItemSessionStorage("name", loginState?.name);
-      router.push(`/pro/user/dashboard?message=${loginState?.message}`);
-    } else if (loginState?.role === Role.super) {
-      setItemSessionStorage("role", Role.super);
-      setItemSessionStorage("token", loginState?.token);
-      setItemSessionStorage("email", loginState?.username);
-      setItemSessionStorage("name", loginState?.name);
-      router.push(`/super/dashboard?message=${loginState?.message}`);
-    } else if (loginState?.role === Role.supplier) {
-      setItemSessionStorage("role", Role.supplier);
-      setItemSessionStorage("token", loginState?.token);
-      setItemSessionStorage("email", loginState?.username);
-      setItemSessionStorage("name", loginState?.name);
-      router.push(`/supplier/dashboard?message=${loginState?.message}`);
+      setItemSessionStorage("token", router?.query?.token);
+      setItemSessionStorage("email", router?.query?.username);
+      setItemSessionStorage("name", router?.query?.name);
+      router.push(`/pro/user/myfolder?message=${router?.query?.message}`);
     }
-  }, [router, loginState]);
+  }, [router]);
+
+  return <PageChange />;
 };
 
 export default Lti;
