0de46f02bd5e6e4d28f84d10eb333a1ebc72bbf0
Admin <vidyam242002>
2024-01-19 16:00:32 +0530

    ticketing system stage 2

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 const runtimeCaching = require("next-pwa/cache");
-const withPWA = require("next-pwa")({
+const withPWA = require("next-pwa");
+
+const nextConfig = withPWA({
+  pwa: {
     dest: "public",
     register: true,
     skipWaiting: true,
-    runtimeCaching,
-    buildExcludes: [/middleware-manifest.json$/],
-});
-
-const nextConfig = withPWA({
-	
+    disable: process.env.NODE_ENV === 'development',
+    runtimeCaching
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 });
 module.exports = nextConfig;
